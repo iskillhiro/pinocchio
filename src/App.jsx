f3c0ef42ba635c878d0bedd6e79e5410c248e767
iskillhiro <155888748+iskillhiro@users.noreply.github.com>
2024-06-30 18:37:42 +0300

    Frontend Pinocchio Netlify Updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,30 +8,27 @@ import Stats from './components/screens/Stats/Stats'
 import Tasks from './components/screens/Tasks/Tasks'
 import Wallet from './components/screens/Wallet/Wallet'
 
-const tg = window.Telegram.WebApps
-
 const App = () => {
 	useEffect(() => {
-		tg.ready()
+		if (window.Telegram && window.Telegram.webApp) {
+			window.Telegram.webApp.ready()
+		} else {
+			console.error('Telegram Web App is not available')
+		}
 	}, [])
 
-	const onclose = () => {
-		tg.close()
-	}
 	return (
-		<>
-			<BrowserRouter>
-				<Routes>
-					<Route path='/' element={<Main />} />
-					<Route path='/coins' element={<Coins />} />
-					<Route path='/referrals' element={<Referrals />} />
-					<Route path='/stats' element={<Stats />} />
-					<Route path='/tasks' element={<Tasks />} />
-					<Route path='/wallet' element={<Wallet />} />
-					<Route onClick={onclose} path='/boosts' element={<Boosts />} />
-				</Routes>
-			</BrowserRouter>
-		</>
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Main />} />
+				<Route path='/coins' element={<Coins />} />
+				<Route path='/referrals' element={<Referrals />} />
+				<Route path='/stats' element={<Stats />} />
+				<Route path='/tasks' element={<Tasks />} />
+				<Route path='/wallet' element={<Wallet />} />
+				<Route path='/boosts' element={<Boosts />} />
+			</Routes>
+		</BrowserRouter>
 	)
 }
 
