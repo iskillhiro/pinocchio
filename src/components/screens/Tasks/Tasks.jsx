8aba4bd065b11175c48676e7a19b9698387d32dc
iskillhiro <155888748+iskillhiro@users.noreply.github.com>
2024-07-03 21:03:25 +0300

    Frontend Pinocchio Netlify Updated

--- src/components/screens/Tasks/Tasks.jsx
+++ src/components/screens/Tasks/Tasks.jsx
@@ -1,37 +1,79 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import axiosDB from '../../../utils/axios/axiosConfig'
 import Navigation from '../../ui/Navigation/Navigation'
+import Loading from '../Loading/Loading'
 import './Tasks.css'
 
 const Tasks = () => {
-	const taskData = [
-		{ name: 'Follow Command', reward: '200.000' },
-		{ name: 'Follow Command', reward: '200.000' },
-		{ name: 'Follow Command', reward: '200.000' },
-		{ name: 'Follow Command', reward: '200.000' },
-		{ name: 'Follow Command', reward: '200.000' },
-		{ name: 'Follow Command', reward: '200.000' },
-		{ name: 'Follow Command', reward: '200.000' },
-	]
+	const [taskData, setTaskData] = useState(null) // Изменено с [] на null
+	const [loading, setLoading] = useState(true)
+
+	const telegramId = '1145622789'
+	useEffect(() => {
+		const getTasks = async () => {
+			try {
+				const response = await axiosDB.get(`/tasks/${telegramId}`)
+				setTaskData(response.data)
+				console.log('TASKDATA: ', response.data)
+			} catch (error) {
+				console.error('Error fetching tasks:', error)
+			} finally {
+				setLoading(false)
+			}
+		}
+
+		getTasks()
+	}, [telegramId])
+
+	if (loading) {
+		return <Loading />
+	}
+
+	// Проверка наличия данных перед рендерингом
+	if (!taskData) {
+		return <div>No tasks available</div>
+	}
 
 	return (
 		<div className='container'>
-			<h1 className='main-title'>tasks</h1>
+			<h1 className='main-title'>Tasks</h1>
 			<div className='tasks-content'>
-				{taskData.map((task, index) => (
+				{taskData.incompleteTasks?.map((task, index) => (
 					<div key={index} id='task-container'>
 						<div className='block tasks'>
 							<div className='task'>
 								<div id='task-block'>
 									<div className='task-logo'></div>
 									<div id='task-info'>
-										<p id='name'>{task.name}</p>
-										<p id='reward'>{task.reward}</p>
+										<p id='name'>{task.taskType}</p>
+										<p id='reward'>
+											{task.isComplete ? 'Completed' : task.reward}
+										</p>
 									</div>
 								</div>
 								<div className='row icon'></div>
 							</div>
 						</div>
 					</div>
+				))}
+				{taskData.completedTasks?.map((task, index) => (
+					<Link to={task.link} key={index} id='task-container'>
+						<div className='block tasks'>
+							<div className='task'>
+								<div id='task-block'>
+									<div className='task-logo'></div>
+									<div id='task-info'>
+										<p id='name'>{task.taskType}</p>
+										<p id='reward'>
+											{task.isComplete ? 'Completed' : task.reward}
+										</p>
+									</div>
+								</div>
+								<div className='row icon'></div>
+							</div>
+						</div>
+					</Link>
 				))}
 			</div>
 			<Navigation />
