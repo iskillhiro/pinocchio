3cb30f50717196857c7c08d1470fb8cc1ad4277d
iskillhiro <155888748+iskillhiro@users.noreply.github.com>
2024-08-01 14:37:51 +0300

    update boostUpgrade

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,8 +1,9 @@
 export const getId = () => {
 	let tg = window.Telegram.WebApp
-	const TELEGRAM_ID = tg.initData.user.id
+	if (tg) {
+		const TELEGRAM_ID = tg.initData.user.id
+		return TELEGRAM_ID
+	}
 	// const TELEGRAM_ID = '1145622789'
 	// const TELEGRAM_ID = '6412147982'
-
-	return TELEGRAM_ID
 }
