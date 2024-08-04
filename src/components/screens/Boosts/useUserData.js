5bc5a1f9a156544617abcd98affbdfbd51aadea9
iskillhiro <155888748+iskillhiro@users.noreply.github.com>
2024-08-04 05:08:51 +0300

    update boosts page add react query

--- src/components/screens/Boosts/useUserData.js
+++ src/components/screens/Boosts/useUserData.js
@@ -2,15 +2,17 @@ import { useQuery } from 'react-query'
 import axiosDB from '../../../utils/axios/axiosConfig'
 import { getId } from '../../../utils/config'
 
+// Function to fetch user data from the API
 const fetchUserData = async telegramId => {
 	const { data } = await axiosDB.get(`/boost/${telegramId}`)
 	return data.userData[0]
 }
 
+// Custom hook to use user data in a React component
 export const useUserData = () => {
 	const telegramId = getId()
 	return useQuery(['userData', telegramId], () => fetchUserData(telegramId), {
-		staleTime: 60000, // 1 minute
-		cacheTime: 300000, // 5 minutes
+		staleTime: 60000, // Data is considered fresh for 1 minute
+		cacheTime: 300000, // Data is cached for 5 minutes
 	})
 }
