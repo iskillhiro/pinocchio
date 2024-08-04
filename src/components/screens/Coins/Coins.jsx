5304907228733676669c680a359e407b14034851
iskillhiro <155888748+iskillhiro@users.noreply.github.com>
2024-08-04 03:58:34 +0300

    update referral page

--- src/components/screens/Coins/Coins.jsx
+++ src/components/screens/Coins/Coins.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import React, { useEffect } from 'react'
 import bronzeCoin from '../../../assets/pictures/coins/bronze/coin_front.svg'
 import goldenCoin from '../../../assets/pictures/coins/golden/coin_front.svg'
 import treeIcon from '../../../assets/pictures/tree.svg'
@@ -11,29 +12,53 @@ import './Coins'
 import './Coins.css'
 import ProgressBar from './ProgressBar'
 import style from './interface.module.css'
+
 const tg = window.Telegram.WebApp
 
-const Coins = () => {
-	const [loading, setLoading] = useState(true)
-	const [process, setProcess] = useState(false)
-	const [displayedBalance, setDisplayedBalance] = useState(0)
-	const telegramId = getId()
-	const [user, setUser] = useState({})
+const fetchUserData = async telegramId => {
+	const { data } = await axiosDB.get(`/user/${telegramId}`)
+	return data
+}
 
-	const fetchUserData = async () => {
-		try {
-			const response = await axiosDB.get(`/user/${telegramId}`)
-			setUser(response.data)
-		} catch (error) {
-			console.error('Error fetching user data:', error)
-		} finally {
-			setLoading(false)
-		}
-	}
+const plantCoin = async telegramId => {
+	await axiosDB.post('/tree/plant', { telegramId })
+}
+
+const startLanding = async telegramId => {
+	await axiosDB.put('/tree/start', { telegramId })
+}
+
+const claim = async telegramId => {
+	await axiosDB.get(`/tree/claim/${telegramId}`)
+}
+
+const Coins = () => {
+	const telegramId = getId()
+	const queryClient = useQueryClient()
+
+	const {
+		data: user,
+		isLoading,
+		isFetching,
+	} = useQuery(['user', telegramId], () => fetchUserData(telegramId), {
+		refetchInterval: 10000,
+	})
+
+	const plantCoinMutation = useMutation(() => plantCoin(telegramId), {
+		onSuccess: () => queryClient.invalidateQueries(['user', telegramId]),
+	})
+
+	const startLandingMutation = useMutation(() => startLanding(telegramId), {
+		onSuccess: () => queryClient.invalidateQueries(['user', telegramId]),
+	})
+
+	const claimMutation = useMutation(() => claim(telegramId), {
+		onSuccess: () => queryClient.invalidateQueries(['user', telegramId]),
+	})
 
 	const incrementBalance = () => {
 		let start = displayedBalance
-		let end = user.tree.lootBalance
+		let end = user?.tree?.lootBalance || 0
 		let increment = (end - start) / 30
 		let interval = 10
 
@@ -50,19 +75,12 @@ const Coins = () => {
 	}
 
 	useEffect(() => {
-		fetchUserData()
-		const intervalId = setInterval(fetchUserData, 10000)
-
-		return () => clearInterval(intervalId)
-	}, [telegramId])
-
-	useEffect(() => {
-		if (user.tree) {
+		if (user?.tree) {
 			incrementBalance()
 		}
-	}, [user.tree])
+	}, [user?.tree])
 
-	if (loading) {
+	if (isLoading) {
 		return (
 			<div className='loader-container'>
 				<Loader />
@@ -71,14 +89,16 @@ const Coins = () => {
 	}
 
 	const getActiveBoostsCount = () => {
-		return user.treeCoinBoosts.reduce(
-			(count, boost) => count + (boost.status ? 1 : 0),
-			0
+		return (
+			user?.treeCoinBoosts.reduce(
+				(count, boost) => count + (boost.status ? 1 : 0),
+				0
+			) || 0
 		)
 	}
 
 	const renderButton = () => {
-		if (user.tree.isActive) {
+		if (user?.tree?.isActive) {
 			if (
 				user.tree.landingEndDate &&
 				new Date(user.tree.landingEndDate) > Date.now()
@@ -90,15 +110,21 @@ const Coins = () => {
 				)
 			} else {
 				return (
-					<button onClick={claim} className='gradient-btn'>
-						{process ? <Loader /> : 'Claim'}
+					<button
+						onClick={() => claimMutation.mutate()}
+						className='gradient-btn'
+					>
+						{claimMutation.isLoading ? <Loader /> : 'Claim'}
 					</button>
 				)
 			}
-		} else if (user.tree.coinPlanted > 0) {
+		} else if (user?.tree.coinPlanted > 0) {
 			return (
-				<button onClick={startLanding} className='gradient-btn'>
-					{process ? <Loader /> : 'Plant'}
+				<button
+					onClick={() => startLandingMutation.mutate()}
+					className='gradient-btn'
+				>
+					{startLandingMutation.isLoading ? <Loader /> : 'Plant'}
 				</button>
 			)
 		} else {
@@ -110,62 +136,6 @@ const Coins = () => {
 		}
 	}
 
-	const updateUserData = async () => {
-		try {
-			const response = await axiosDB.get(`/user/${telegramId}`)
-			setUser(response.data)
-		} catch (error) {
-			console.error('Error updating user data:', error)
-		}
-	}
-
-	const plantCoin = async () => {
-		if (!user.tree.isActive) {
-			try {
-				const response = await axiosDB.post('/tree/plant', {
-					telegramId: user.telegramId,
-				})
-				tg.HapticFeedback.impactOccurred('light')
-				if (response.status === 200) {
-					updateUserData()
-				}
-			} catch (error) {
-				console.error('Error planting coin:', error)
-			}
-		}
-	}
-
-	const startLanding = async () => {
-		setProcess(true)
-		try {
-			const response = await axiosDB.put('/tree/start', {
-				telegramId: user.telegramId,
-			})
-			tg.HapticFeedback.impactOccurred('light')
-			if (response.status === 200) {
-				updateUserData()
-			}
-		} catch (error) {
-			console.error('Error starting landing:', error)
-		} finally {
-			setProcess(false)
-		}
-	}
-
-	const claim = async () => {
-		setProcess(true)
-		try {
-			const response = await axiosDB.get(`/tree/claim/${user.telegramId}`)
-			tg.HapticFeedback.impactOccurred('light')
-			if (response.status === 200) {
-				updateUserData()
-			}
-		} catch (error) {
-			console.error('Error claiming coins:', error)
-		} finally {
-			setProcess(false)
-		}
-	}
 	return (
 		<div className='container coins'>
 			<h1 className='title coins gradient up-case no-wrap fade-in'>
@@ -179,7 +149,7 @@ const Coins = () => {
 			</h1>
 			<div className={style.tree_zone}>
 				{Array.from(
-					{ length: Math.min(user.tree.coinPlanted, 7) },
+					{ length: Math.min(user?.tree.coinPlanted || 0, 7) },
 					(_, index) => (
 						<div key={index} className={`${style.coin} active fade-in`}>
 							<img src='./zecchino.svg' alt='coin' />
@@ -187,7 +157,7 @@ const Coins = () => {
 					)
 				)}
 				<img
-					onClick={plantCoin}
+					onClick={() => plantCoinMutation.mutate()}
 					className='fade-in main-tree pointer'
 					src={treeIcon}
 					alt='tree'
@@ -196,7 +166,7 @@ const Coins = () => {
 
 			<div id='planted-coins'>
 				<div id='coin-count'>
-					<p id='count'>{user.tree.coinPlanted}</p>
+					<p id='count'>{user?.tree.coinPlanted || 0}</p>
 					<div className='icon'>
 						<img src={goldenCoin} alt='golden coin' />
 					</div>
@@ -205,14 +175,14 @@ const Coins = () => {
 					+<p id='count'>{getActiveBoostsCount() * 50}</p>%
 				</div>
 			</div>
-			{user.tree.isActive && (
+			{user?.tree?.isActive && (
 				<ProgressBar
 					min={new Date(user.tree.landingStartDate)}
 					max={new Date(user.tree.landingEndDate)}
 				/>
 			)}
 			<div className='tools'>
-				{user.treeCoinBoosts.map((boost, index) => {
+				{user?.treeCoinBoosts.map((boost, index) => {
 					const iconPath = require(`/src/assets/pictures/${boost.icon}`)
 					return (
 						<div
